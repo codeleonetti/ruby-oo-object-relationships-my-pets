bdcef4a39598fd8c0e559b0f81e2dec02509e1be
john <john.d.leonetti@gmail.com>
2021-03-09 12:41:28 -0800

    commit tree 2d83cdc3a3503ca93fb49f21cb0d7187e83c7f45

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,27 @@
+require "pry"
 class Dog
   # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name, owner)
+    @owner = owner
+    @name = name
+    @mood = "nervous"
+    @@all << self
+
+  end
+
+  def self.all
+    @@all
+  end
+
+
+
+
+
+
+
 end
